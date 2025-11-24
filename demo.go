ac1d182ff775149377bc4cdf42a8bdfca61efda6
mohamadreza <mohamadrezamomen97@gmail.com>
2025-11-24 11:19:18 +0330

    feat: initailize

---
+++ demo.go
@@ -0,0 +1,47 @@
+// Package plugindemo a demo plugin.
+package mamad
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+// Config the plugin configuration.
+type Config struct {
+	Headers map[string]string `json:"headers,omitempty"`
+}
+
+// CreateConfig creates the default plugin configuration.
+func CreateConfig() *Config {
+	return &Config{
+		Headers: make(map[string]string),
+	}
+}
+
+// Demo a Demo plugin.
+type Demo struct {
+	next     http.Handler
+	headers  map[string]string
+	name     string
+	template *template.Template
+}
+
+// New created a new Demo plugin.
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	if len(config.Headers) == 0 {
+		return nil, fmt.Errorf("headers cannot be empty")
+	}
+
+	return &Demo{
+		headers:  config.Headers,
+		next:     next,
+		name:     name,
+		template: template.New("demo").Delims("[[", "]]"),
+	}, nil
+}
+
+func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	rw.Write([]byte("hellllllo"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mohamadrezamomeni/mamad
+
+go 1.19
